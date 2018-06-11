18347637ff20789950cba1eb1b004b9db2ad9cf5
NatalieBlack <natalievictoriablack@gmail.com>
2018-06-10 22:55:13 -0400

    Only save words if all characters are in db (ignore half width ones).

--- jmdict.py
+++ jmdict.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "font_literacy.settings")
@@ -14,25 +16,38 @@ tree = ET.parse(filename)
 root = tree.getroot()
 
 json_data = []
+words_json = []
+char_occurs_json = []
 word_count = 0
 char_count = 0
 
-for word in root.iter('reb'):
 
-  word_count += 1
+
+for word in root.iter('reb'):
+  save = True
+
   fields = {"learned": False}
 
   word_instance = {"model": "quiz.word", "pk": word_count, "fields": fields}
-  json_data.append(word_instance)
 
   for i, c in enumerate(list(word.text)):
     char_count += 1
 
-    char = Character.get(value=c)
-    fields = {"position": i, "character": char, "word": word_count}
-    char_occur = { "model": "quiz.CharacterOccurrence", "pk": char_count, "fields": fields}
-    json_data.append(char_occur)
+    matches = Character.objects.filter(value=c)
+    if len(matches) > 0:
+      char = matches[0]
+      fields = {"position": i, "character": char.pk, "word": word_count}
+      char_occur = { "model": "quiz.CharacterOccurrence", "pk": char_count, "fields": fields}
+      char_occurs_json.append(char_occur)
+    else:
+      save = False
+      break
+
+  if save:
+    words_json.append(word_instance)
+    word_count += 1
 
 
 
+json_data = words_json + char_occurs_json
 print json.dumps(json_data)
